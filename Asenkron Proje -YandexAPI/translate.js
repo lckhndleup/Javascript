db4115548fefb94be97df9c5db124f22aed5c05f
mehmet ali <67506638+lckhndleup@users.noreply.github.com>
2023-02-04 20:29:37 +0300

    Update translate.js

--- Asenkron Proje -YandexAPI/translate.js
+++ Asenkron Proje -YandexAPI/translate.js
@@ -2,7 +2,7 @@
 
 function Translate(word,language){
     
-    this.apikey ="trnsl.1.1.20180930T080756Z.753c49142579b043.b2798189b8760e7b357c9d23a8736ef0a54be481";
+    this.apikey ="ENTER YOUR YANDEX TRANSLATE API KEY";
     this.word = word ;
     this.language = language;
     this.xhr = new XMLHttpRequest();
