e425d9447d26f8fb0ed431bad15d9ac58c9aaba9
mehmet ali <67506638+lckhndleup@users.noreply.github.com>
2023-02-04 20:28:36 +0300

    Update app.js

--- Asenkron Proje -YandexAPI/app.js
+++ Asenkron Proje -YandexAPI/app.js
@@ -1,26 +1,21 @@
-//prototype , ajax ve callback kullanarak işlemri yapıcaz.
+//prototype , ajax ve callback .
 
 eventListeners();
 
 function eventListeners(){
 
-//butona bastıgımızda kelimemizi çevirmek istiyoruz.forma submit yapıldıgında kelimeyi çevirmek isitiyoruz.
     document.getElementById("translate-form").addEventListener("submit",translateWord);
-    //change eventi 
+    //change event 
     document.getElementById("language").onchange = function(){
-        //select listemizdeki değerler değiştikçe eventimiz tetiklenecek.
         console.log("event ")
         ui.changeUI();
     }
 
 }
 
-//Translate constructordan bir tane obje üretelim.
+
 const translate = new Translate(document.getElementById("word").value,document.getElementById
-("language").value); //word ve language parametresi alır.bunların anlık değerlerini alalım.
-
-//ui objesi oluşturma
-
+("language").value); 
 const ui = new UI();
 
 function translateWord(event){
@@ -28,22 +23,19 @@ function translateWord(event){
     translate.changeParameters(document.getElementById("word").value,document.getElementById("language").value);
 
     translate.translateWord(function(err,response){
-        //translateWord functionı bir tane callback alıyor.içine bir callback yazıcaz.
-        //bu fonksiyon bize 2 tane değer döner eğer hata yoksa error null olacak. ve hata olmaazsa çevrilen kelime direk olarak gelecek.ancak hata varsa hata oluştu şeklinde gelecek ve response null olacak.
         if(err){ 
+            //error
             console.log(err);
-            //hata olmazsa response u yazdıralım.
+            
         
         }
         else{
-            //hata olursa 
+            //not error
             ui.displayTranslate(response);
         }
 
     }); 
 
     event.preventDefault();
-
-    //sayfa yenilenmesini önlemek için event.preventDefault()
 }
 
